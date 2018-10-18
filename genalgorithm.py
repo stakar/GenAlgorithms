29090d6bd5055068e364d1e6ac72772f83b37cd7
Stanislaw Karkosz <stakarkosz@gmail.com>
2018-10-18 13:10:19 +0200

    Created genalgorithm.py, the original file containing code

---
+++ genalgorithm.py
@@ -0,0 +1,8 @@
+#This file is containing a simple genetic algorithm prepared for MD seminary
+#presentation
+
+import numpy as np
+
+aim = "Programming is awesome!"
+
+chromosome = np.array(len(aim),dtype='U')
