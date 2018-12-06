b3975074d826832e4687368fb0d803c4faefd31c
Stanislaw Karkosz <stakarkosz@gmail.com>
2018-12-06 18:24:11 +0100

    Added new versions of old functions, like transform, descendants_generation and random_mutation

--- genalgorithm03.py
+++ genalgorithm03.py
@@ -3,145 +3,119 @@ from string import ascii_letters, punctuation
 
 class GenAlgorithmString(object):
 
-    def __init__(self,symbols = (ascii_letters + punctuation + ' '),n_population=10,
-                 n_generation = 100,method = 'roulette',K=4,desired_fitness=0.4):
+    def __init__(self,symbols = (ascii_letters + punctuation + ' '),
+                 n_population=10,method = 'roulette',K=4,desired_fitness=0.4,
+                 mutation_probability = 0.02):
 
         self = self
         self.symbols = symbols
         self.n_population = n_population
-        self.n_generation = n_generation
         self.method = method
         self.K = K
         self.desired_fitness = desired_fitness
+        self.mutation_probability = mutation_probability
 
     def get_symbol(self):
         """ Generates random symbol """
         return self.symbols[np.random.randint(len(self.symbols))]
 
     def fit(self,aim):
-        """ Fits the given sentence to the model. Input aim
-        """
-        target = np.array([n for n in aim],dtype = 'U1')
-        self.target = target
-        self.n_genotype = len(target)
-        self.population = np.zeros((self.n_population,self.n_genotype),
-                                    dtype = 'U1')
+        """ Fits the given sentence to the mod el. Input aim is the string, sente
+        nce that the algorithm is supposed to found."""
+        self.target = np.array([n for n in aim],dtype = 'U1')
+        self.n_genotype = len(self.target)
+        self.population = np.array([[self.get_symbol() for
+         n in range(self.n_genotype)] for n in range(self.n_population)])
 
-    def transform(self):
-        """ Performs the whole algorithm until desired fitness is reached. """
-        self._mutate_population()
-        pop = np.max(self._population_fitness())
+    def transform():
+        """ Transform, i.e. execute an algorithm. """
+        best_fitness = np.min(self._population_fitness())
         self.n_generation = 0
-        while (pop < self.desired_fitness):
-            self.descendants_generation()
+        while best_fitness < self.desired_fitness:
+            self(self.descendants_generation())
             self.n_generation += 1
-            pop = np.max(self._population_fitness())
-
-    def _mutate_population(self):
-        """ Mutates the whole population, for each individual generates an array
-        of characers """
-        for individual in range(self.n_population):
-            self.population[individual] = [self.get_symbol() for n in
-                                           range(self.n_genotype)]
+            best_fitness = np.min(self._population_fitness())
 
 
     def _check_fitness(self,chromosome,target):
         """ Checks the fitness of individual. Fitness is the mesure of distance
         between letter generated randomly and the one that maps it on the locus
         in target array"""
-        return sum([(ord(chromosome[n])-ord(target[n])) for n in range(
+        return sum([abs(ord(chromosome[n])-ord(target[n])) for n in range(
                        self.n_genotype)])
 
+    def _population_fitness(self):
+        """Returns an arrray with fitness of each individual in population"""
+        return np.array([self._check_fitness(n,self.target) for
+                         n in self.population])
 
     @staticmethod
-    def _pairing(parents):
+    def _pairing(mother,father):
         """ Method for pairing chromosomes and generating descendants, array of
         characters with shape [2,n_genotype] """
-        children = np.zeros((2,parents.shape[1]),dtype = 'U1')
-        n_heritage = np.random.randint(0,parents[0].shape[0])
-        children[0] = np.concatenate([parents[0][:n_heritage],
-                                      parents[1][n_heritage:]])
-        children[1] = np.concatenate([parents[1][:n_heritage],
-                                      parents[0][n_heritage:]])
-        return children
-
-    def _population_fitness(self):
-        "Returns an arrray with fitness of each individual in population"
-        return [self._check_fitness(n,self.target) for n in self.population]
-
-    def random_mutation(self):
-        """ Randomly mutate population """
-        N,C = self.population.shape
-        new_population = self.population.copy()
-        n_mutations = round(self.population.size * 0.02)
-        for n in range(n_mutations):
-            new_population[np.random.randint(N),
-                           np.random.randint(C)] = self.get_symbol()
-        self.population = new_population
+        n_heritage = np.random.randint(0,len(mother))
+        child1 = np.concatenate([father[:n_heritage],mother[n_heritage:]])
+        child2 = np.concatenate([mother[:n_heritage],father[n_heritage:]])
+        return child1,child2
 
     def descendants_generation(self):
-        """ Generaters new population from pairing old one """
-        fitness = self._population_fitness()
-        if np.any(fitness != 0):
-            if self.method == 'ranking':
-                bests = self._ranking()
-            elif self.method == 'roulette':
-                bests = self.roulette()
-            self._mutate_population()
-            for n in range(round(self.K/2)):
-                parent1 = bests[np.random.randint(self.K)]
-                parent2 = bests[np.random.randint(self.K)]
-                descendants = self._pairing(np.array([parent1,parent2]))
-                self.population[(n*2)] = descendants[0].squeeze()
-                self.population[(n*2)+1] = descendants[1].squeeze()
-                # self.random_mutation()
-        else:
-            self._mutate_population()
-
+        """ Selects the best individuals, then generates new population, with
+        half made of parents (i.e. best individuals) and half children(descendan
+        ts of parents) """
+        parents_pop = self.roulette()
+        n_parents = int(np.round(self.n_population/3))
+        for n in range(n_parents,self.n_population-1):
+            father = parents_pop[np.random.randint(self.n_population)]
+            mother = parents_pop[np.random.randint(self.n_population)]
+            children = self._pairing(mother,father)
+            self.population[(n)] = children[0]
+            self.population[(n)+1] = children[1]
+        self.population[:n_parents] = parents_pop[:n_parents]
 
     def roulette_wheel(self):
-        """ Generates roulette wheel based on fitness of each individual.
-            Needed for roulette selection of individuals"""
-        generation = self._population_fitness()
-        probability = 0
-        wheel = np.zeros(3)
-        if np.any(generation != 0):
-            for individual in range(self.n_population):
-                if generation[individual] > 0:
-                    ind_probability = probability + (
-                    generation[individual] / np.sum(generation))
-                    wheel = np.vstack([wheel,[individual,
-                                       probability,ind_probability]])
-                    probability = probability + (
-                                  generation[individual] / np.sum(generation))
-            print(wheel)
-            return wheel[1:,:]
+        """ Method that returns roulette wheel, an array with shape [n_populatio
+        n, low_individual_probability,high_individual_probability]"""
+        max_val = 126*self.n_genotype
+        pop_fitness = [max_val-n for n in self._population_fitness()]
+        wheel = np.zeros((self.n_population,3))
+        prob = 0
+        for n in range(self.n_population):
+            ind_prob = prob + (pop_fitness[n] / np.sum(pop_fitness))
+            wheel[n] = [n,prob,ind_prob]
+            prob = ind_prob
+        return wheel
 
-    @staticmethod
-    def roulette_swing(wheel):
-        """ Swings the roulette wheel.
-            Needed for roulette selection of individuals """
+    def roulette_swing(self,wheel):
+        """ This method takes as an inpute """
         which = np.random.random()
         for n in range(len(wheel)):
             if which > wheel[n][1] and which < wheel[n][2]:
                 return int(wheel[n][0])
 
     def roulette(self):
-        """ Performs roulette selection on population """
+        """ This method performs selection of individuals, it takes the coeffici
+        ent k, which is number of new individuals """
         wheel = self.roulette_wheel()
-        winners = np.zeros((self.K,self.population.shape[1]),dtype='U1')
-        for n in range(self.K):
-            which = self.roulette_swing(wheel)
-            winners[n] = self.population[which]
-        return winners
+        return np.array([self.population[self.roulette_swing(wheel)]
+                         for n in range(self.n_population)])
 
+    def random_mutation(self):
+        population = self.population.copy()
+        for n in range(self.n_population):
+            decision = np.random.random()
+            if decision < self.mutation_probability:
+                which_gene = np.random.randint(self.n_genotype)
+                population[n][which_gene] = self.get_symbol()
+        self.population = population
 if __name__ == '__main__':
-    genalg = GenAlgorithmString(symbols = (ascii_letters),n_generation = 0,
-                                K=5,n_population=10,method = 'roulette',
-                                desired_fitness = 50)
+    genalg = GenAlgorithmString(K=5,n_population=5,method = 'roulette',
+                                desired_fitness = 50,mutation_probability=0.02)
     genalg.fit('Programming is awesome')
-    genalg.transform()
-    max_fitness = np.max(genalg._population_fitness())
-    best_individual = genalg.population[np.where(max_fitness)]
-    print('Number of generations: %i \nBest fitness: %f' %(genalg.n_generation,
-    max_fitness))
+    pop1 = genalg.population
+    print(pop1)
+    genalg.random_mutation()
+    # genalg.descendants_generation()
+    pop2 = genalg.population
+    print('CHildren:')
+    print(pop2)
+    print(pop1 == pop2)
